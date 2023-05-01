4abc5dcf5c91a4a0bc435abcc15458e0fa20b115
Sandeep kumar <sandeeper96@gmail.com>
2023-05-01 09:23:28 +0530

    update

--- src/components/sections/loginPages/Login.js
+++ src/components/sections/loginPages/Login.js
@@ -8,7 +8,19 @@ export default function Login() {
         alt="Little Lemon Ingredients"
       ></img>
       <div className="reserve-header-text">
-        <h1>Login</h1>
+        <div style={{display:"flex"}}>
+          <label htmlFor="">User Name:</label>
+          <input type="text" name="" id="" placeholder="Enter User Name" />
+          </div>
+          <div>
+          <label htmlFor="">Password</label>
+          <input type="text" placeholder="Enter Your" />
+          </div>
+          <div>
+            <button className="login-but">Login</button>
+          </div>
+          
+        
       </div>
     </header>
   );
